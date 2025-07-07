a3bebe9279b9cc60424ca4eca23fb47756f610ef
Shihong Xu <shawnhsu@live.com>
2025-07-07 13:35:01 +0930

    debug

--- assets/index-Dx0xWov-.js
+++ assets/index-Dx0xWov-.js
@@ -1,1 +1,96 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function d(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=d(e);fetch(e.href,t)}})();const i="uniqid",w="nodeVideo",b={iceServers:[]},l=cast.framework.CastReceiverContext.getInstance(),g=l.getPlayerManager(),f=document.querySelector("video"),o="urn:x-cast:oeuvre.signal",c=l.getCurrentSession(),p=({isLoading:a})=>{a?document.querySelector(".loader").style.display="block":document.querySelector(".loader").style.display="none"};f.addEventListener("loadeddata",()=>p({isLoading:!1}));g.setMessageInterceptor(cast.framework.messages.MessageType.LOAD,a=>{a.media.entity||(a.media.entity=a.media.contentId),p({isLoading:!0});const s=new RTCPeerConnection(b);return l.addCustomMessageListener(o,async d=>{const r={type:"echo",data:d.data};switch(c.sendMessage(o,r),r.type){case"offer":const e=r.data.offer;console.log("offer"),await s.setRemoteDescription(e);const t=await s.createAnswer();await s.setLocalDescription(t),c.sendMessage(o,{type:"answer",data:{answer:t,roomId:i}});break;case"ice-candidate":const n=r.data.candidate;c.sendMessage(o,{type:"ice-candidate",data:{candidate:n,roomId:i}}),console.log("ice-candidate");try{await s.addIceCandidate(n)}catch(L){console.error("Error adding received ice candidate",L)}break}}),new Promise((d,r)=>{s.ontrack=e=>{if(e.streams&&e.streams[0]){const t=document.getElementById(w);t.srcObject=e.streams[0],d(e)}},s.onicecandidate=e=>{e.candidate&&c.sendMessage(o,{type:"ice-candidate",candidate:e.candidate,roomId:i})},c.sendMessage(o,{type:"join-room",data:{roomId:i}}),c.sendMessage(o,{type:"request",data:{event:"webrtc",roomId:i}})})});const y=({shouldPlay:a})=>s=>(f[a?"play":"pause"](),g.broadcastStatus(!0),s);g.setMessageInterceptor(cast.framework.messages.MessageType.PAUSE,y({shouldPlay:!1}));g.setMessageInterceptor(cast.framework.messages.MessageType.PLAY,y({shouldPlay:!0}));const m=new cast.framework.CastReceiverOptions;m.skipPlayersLoad=!0;m.disableIdleTimeout=!0;m.supportedCommands=cast.framework.messages.Command.ALL_BASIC_MEDIA;const u=cast.debug.CastDebugLogger.getInstance();l.addEventListener(cast.framework.system.EventType.READY,()=>{u.setEnabled(!0),u.showDebugLogs(!0),u.clearDebugLogs(),u.setLevel(cast.debug.LoggerLevel.DEBUG)});l.start(m);
+;(function () {
+  const a = document.createElement('link').relList
+  if (a && a.supports && a.supports('modulepreload')) return
+  for (const e of document.querySelectorAll('link[rel="modulepreload"]')) o(e)
+  new MutationObserver((e) => {
+    for (const t of e)
+      if (t.type === 'childList')
+        for (const c of t.addedNodes) c.tagName === 'LINK' && c.rel === 'modulepreload' && o(c)
+  }).observe(document, { childList: !0, subtree: !0 })
+  function s(e) {
+    const t = {}
+    return (
+      e.integrity && (t.integrity = e.integrity),
+      e.referrerPolicy && (t.referrerPolicy = e.referrerPolicy),
+      e.crossOrigin === 'use-credentials'
+        ? (t.credentials = 'include')
+        : e.crossOrigin === 'anonymous'
+        ? (t.credentials = 'omit')
+        : (t.credentials = 'same-origin'),
+      t
+    )
+  }
+  function o(e) {
+    if (e.ep) return
+    e.ep = !0
+    const t = s(e)
+    fetch(e.href, t)
+  }
+})()
+const i = 'uniqid',
+  M = { iceServers: [] },
+  l = cast.framework.CastReceiverContext.getInstance(),
+  g = l.getPlayerManager(),
+  u = cast.debug.CastDebugLogger.getInstance(),
+  m = document.querySelector('video'),
+  n = 'urn:x-cast:oeuvre.signal',
+  d = l.getCurrentSession(),
+  y = ({ isLoading: r }) => {
+    document.querySelector('.loader').style.display = r ? 'block' : 'none'
+  }
+m.addEventListener('loadeddata', () => y({ isLoading: !1 }))
+g.setMessageInterceptor(cast.framework.messages.MessageType.READY, (r) => {
+  r.media.entity || (r.media.entity = r.media.contentId),
+    (document.querySelector('.loader').innerHTML = 'loaded'),
+    y({ isLoading: !0 }),
+    u.setEnabled(!0),
+    u.showDebugLogs(!0),
+    u.clearDebugLogs(),
+    u.setLevel(cast.debug.LoggerLevel.DEBUG)
+  const a = new RTCPeerConnection(M)
+  return (
+    l.addCustomMessageListener(n, async (s) => {
+      const o = { category: 'echo', data: s.data }
+      switch ((d.sendMessage(n, o), JSON.parse(s.data).type)) {
+        case 'offer':
+          const t = o.data.offer
+          console.log('offer'), await a.setRemoteDescription(t)
+          const c = await a.createAnswer()
+          await a.setLocalDescription(c),
+            d.sendMessage(n, { type: 'answer', data: { answer: c, roomId: i } })
+          break
+        case 'ice-candidate':
+          const p = o.data.candidate
+          d.sendMessage(n, { type: 'ice-candidate', data: { candidate: p, roomId: i } }),
+            console.log('ice-candidate')
+          try {
+            await a.addIceCandidate(p)
+          } catch (w) {
+            console.error('Error adding received ice candidate', w)
+          }
+          break
+      }
+    }),
+    (a.onicecandidate = (s) => {
+      s.candidate && d.sendMessage(n, { type: 'ice-candidate', candidate: s.candidate, roomId: i })
+    }),
+    new Promise((s, o) => {
+      ;(document.querySelector('.loader').innerHTML = 'tracking...'),
+        (a.ontrack = (e) => {
+          e.streams && e.streams[0] && ((m.srcObject = e.streams[0]), s(e))
+        }),
+        d.sendMessage(n, { type: 'join-room', data: { roomId: i } }),
+        d.sendMessage(n, { type: 'request', data: { event: 'webrtc', roomId: i } })
+    })
+  )
+})
+const L =
+  ({ shouldPlay: r }) =>
+  (a) => (m[r ? 'play' : 'pause'](), g.broadcastStatus(!0), a)
+g.setMessageInterceptor(cast.framework.messages.MessageType.PAUSE, L({ shouldPlay: !1 }))
+g.setMessageInterceptor(cast.framework.messages.MessageType.PLAY, L({ shouldPlay: !0 }))
+const f = new cast.framework.CastReceiverOptions()
+f.skipPlayersLoad = !0
+f.disableIdleTimeout = !0
+f.supportedCommands = cast.framework.messages.Command.ALL_BASIC_MEDIA
+l.start(f)
