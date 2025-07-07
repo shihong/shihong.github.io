38f710f7fa2ef680854e9237f308a2eafffabf2d
Shihong Xu <shawnhsu@live.com>
2025-07-07 11:30:33 +0930

    debug

--- assets/index-Tn9fxRY2.js
+++ assets/index-Tn9fxRY2.js
@@ -1,1 +1,74 @@
-(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&r(o)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const i="uniqid",g="nodeVideo",l={iceServers:[]},c=window.cast.debug.CastDebugLogger.getInstance(),d=window.cast.receiver.CastReceiverManager.getInstance(),p="urn:x-cast:oeuvre.signal",s=d.getCastMessageBus(p);d.onReady=function(f){c.debugOverlayElement_||(c.setEnabled(!0),c.showDebugLogs(!0),c.clearDebugLogs()),d.setApplicationState("Receiver Loaded");const a=new RTCPeerConnection(l);a.ontrack=n=>{if(n.streams&&n.streams[0]){const r=document.getElementById(g);r.srcObject=n.streams[0]}},a.onicecandidate=n=>{n.candidate&&s.send(JSON.stringify({type:"ice-candidate",candidate:n.candidate,roomId:i}))},s.send(JSON.stringify({type:"join-room",data:{roomId:i}})),s.send(JSON.stringify({type:"request",data:{event:"webrtc",roomId:i}})),s.onMessage=async function(n){const r=JSON.parse(n.data);switch(s.send(JSON.stringify({type:"echo",data:n.data})),r.type){case"offer":const e=r.data.offer;console.log("offer"),await a.setRemoteDescription(e);const t=await a.createAnswer();await a.setLocalDescription(t),s.send(JSON.stringify({type:"answer",data:{answer:t,roomId:i}}));break;case"ice-candidate":const o=r.data.candidate;s.send(JSON.stringify({type:"ice-candidate",data:{candidate:o,roomId:i}})),console.log("ice-candidate");try{await a.addIceCandidate(o)}catch(u){console.error("Error adding received ice candidate",u)}break}}};d.start();
+;(function () {
+  const r = document.createElement('link').relList
+  if (r && r.supports && r.supports('modulepreload')) return
+  for (const e of document.querySelectorAll('link[rel="modulepreload"]')) n(e)
+  new MutationObserver((e) => {
+    for (const t of e)
+      if (t.type === 'childList')
+        for (const s of t.addedNodes) s.tagName === 'LINK' && s.rel === 'modulepreload' && n(s)
+  }).observe(document, { childList: !0, subtree: !0 })
+  function a(e) {
+    const t = {}
+    return (
+      e.integrity && (t.integrity = e.integrity),
+      e.referrerPolicy && (t.referrerPolicy = e.referrerPolicy),
+      e.crossOrigin === 'use-credentials'
+        ? (t.credentials = 'include')
+        : e.crossOrigin === 'anonymous'
+        ? (t.credentials = 'omit')
+        : (t.credentials = 'same-origin'),
+      t
+    )
+  }
+  function n(e) {
+    if (e.ep) return
+    e.ep = !0
+    const t = a(e)
+    fetch(e.href, t)
+  }
+})()
+const i = 'uniqid',
+  u = 'nodeVideo',
+  l = { iceServers: [] },
+  o = window.cast.receiver.CastReceiverManager.getInstance(),
+  p = 'urn:x-cast:oeuvre.signal'
+o.onReady = function (d) {
+  const r = o.getCastMessageBus(p)
+  o.setApplicationState('Receiver Loaded')
+  const a = new RTCPeerConnection(l)
+  ;(a.ontrack = (n) => {
+    if (n.streams && n.streams[0]) {
+      const e = document.getElementById(u)
+      e.srcObject = n.streams[0]
+    }
+  }),
+    (a.onicecandidate = (n) => {
+      n.candidate &&
+        r.send(JSON.stringify({ type: 'ice-candidate', candidate: n.candidate, roomId: i }))
+    }),
+    r.send(JSON.stringify({ type: 'join-room', data: { roomId: i } })),
+    r.send(JSON.stringify({ type: 'request', data: { event: 'webrtc', roomId: i } })),
+    (r.onMessage = async function (n) {
+      const e = JSON.parse(n.data)
+      switch ((r.send(JSON.stringify({ type: 'echo', data: n.data })), e.type)) {
+        case 'offer':
+          const t = e.data.offer
+          console.log('offer'), await a.setRemoteDescription(t)
+          const s = await a.createAnswer()
+          await a.setLocalDescription(s),
+            r.send(JSON.stringify({ type: 'answer', data: { answer: s, roomId: i } }))
+          break
+        case 'ice-candidate':
+          const c = e.data.candidate
+          r.send(JSON.stringify({ type: 'ice-candidate', data: { candidate: c, roomId: i } })),
+            console.log('ice-candidate')
+          try {
+            await a.addIceCandidate(c)
+          } catch (f) {
+            console.error('Error adding received ice candidate', f)
+          }
+          break
+      }
+    })
+}
+o.start()
